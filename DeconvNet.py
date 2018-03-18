13eef67f49899768a96fa12c3f9f67dea93ba597
qianshangbushang <549405579@qq.com>
2018-03-18 15:27:23 +0800

    code commit

--- DeconvNet.py
+++ DeconvNet.py
@@ -17,10 +17,11 @@ class DeconvNet:
         :return:
         """
         import os, wget, tarfile
-        if os.listdir("data") == ['.gitignore']:
+        if os.listdir("data") == []:
+            print("downloading data")
             filenames = ['VOC_OBJECT.tar.gz', 'VOC2012_SEG_AUG', 'stage_1_train_imgset.tar.gz',
                          'stage_2_train_imgset.tar.gz']
-            url = 'http://cvlab.postech.ac.kr/research/deconvnet/data'
+            url = 'http://cvlab.postech.ac.kr/research/deconvnet/data/'
             for filename in filenames:
                 wget.download(url + filename, out=os.path.join('data', filename))
                 tar = tarfile.open(os.path.join('data', filename))
